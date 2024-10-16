7fcb1261631e0be392bd45c906981fae8645b2a0
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2024-10-16 20:35:07 +0300

    Add project files.

---
+++ Cădar_Raul_Lab2/Data/Cădar_Raul_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cădar_Raul_Lab2.Models;
+
+namespace Cădar_Raul_Lab2.Data
+{
+    public class Cădar_Raul_Lab2Context : DbContext
+    {
+        public Cădar_Raul_Lab2Context (DbContextOptions<Cădar_Raul_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cădar_Raul_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Cădar_Raul_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
